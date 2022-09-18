4c48e8e610e581a4115e4977c9332c677389c3ed
ToastbrotFranzi <88054557+ToastbrotFranzi@users.noreply.github.com>
2022-09-18 15:12:54 +0200

    made sometging

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
-#import os
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-#app.config['SECRET_KEY'] = os.environ.get("API_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
